ad878a98e5f8b9c54788c0509940d994f9d8f04c
9049480440 <9049480440ea@gmail.com>
2025-03-25 19:04:27 +0500

    Update sheets.py

--- services/sheets.py
+++ services/sheets.py
@@ -4,30 +4,25 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from config import SPREADSHEET_ID, ACTIVITY_SHEET_NAME
 
-# Настройка доступа к таблице через переменную среды
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
-# Загружаем JSON из переменной окружения (строкой)
+# Подключение через переменную среды
 creds_dict = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 
-# Подключаемся к Google Таблице
 client = gspread.authorize(creds)
 sheet = client.open_by_key(SPREADSHEET_ID).worksheet(ACTIVITY_SHEET_NAME)
 
-# Добавляем или обновляем пользователя
 def add_or_update_user(user):
     user_id = str(user.id)
     all_users = sheet.get_all_values()
-    user_ids = [row[0] for row in all_users[1:]]  # пропускаем заголовок
+    user_ids = [row[0] for row in all_users[1:]]
 
     if user_id in user_ids:
-        # обновим дату последнего входа
         idx = user_ids.index(user_id) + 2
         sheet.update_cell(idx, 4, '=TODAY()')
         sheet.update_cell(idx, 5, 'вход')
     else:
-        # добавим нового пользователя
         new_row = [
             str(user.id),
             user.username or '',

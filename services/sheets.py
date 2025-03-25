41882a92c03ff6329eb5d2c1eea3105ca9267570
9049480440 <9049480440ea@gmail.com>
2025-03-25 20:27:30 +0500

    Update sheets.py

--- services/sheets.py
+++ services/sheets.py
@@ -6,29 +6,20 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from config import SPREADSHEET_ID, ACTIVITY_SHEET_NAME
 
-# Эти scope нужны для работы с Google Sheets и Google Drive
 scope = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/drive"
 ]
 
-# Считываем содержимое JSON из переменной окружения
-creds_json = os.getenv("GOOGLE_CREDENTIALS_JSON")  # То, что вы добавили на Render.com
-
+# Читаем содержимое JSON из переменной окружения
+creds_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
 if not creds_json:
-    # Если по какой-то причине переменная окружения пустая, выводим ошибку
     raise ValueError("GOOGLE_CREDENTIALS_JSON is not set or is empty.")
 
-# Парсим строку как JSON
 creds_dict = json.loads(creds_json)
-
-# Создаём учетные данные сервисного аккаунта из словаря
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 
-# Авторизация через gspread
 client = gspread.authorize(creds)
-
-# Открываем нужную таблицу по ID
 sheet = client.open_by_key(SPREADSHEET_ID).worksheet(ACTIVITY_SHEET_NAME)
 
 def add_or_update_user(user):
@@ -37,11 +28,9 @@ def add_or_update_user(user):
     user_ids = [row[0] for row in all_users[1:]]  # пропускаем заголовок
 
     if user_id in user_ids:
-        # обновим только дату последнего входа
         idx = user_ids.index(user_id) + 2
         sheet.update_cell(idx, 4, '=TODAY()')
     else:
-        # добавим нового пользователя
         new_row = [
             str(user.id),
             user.username or '',

fcdbda6185c92e07f896ee8e2ed98412f9473540
9049480440 <9049480440ea@gmail.com>
2025-03-25 17:20:58 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# main.py
+
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils import executor
+from handlers import user_handlers
+
+from config import BOT_TOKEN
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+# Регистрируем обработчики
+user_handlers.register_handlers(dp)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)

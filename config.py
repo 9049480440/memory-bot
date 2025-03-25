e919ee988f140067ca6715705963b72afd306646
9049480440 <9049480440ea@gmail.com>
2025-03-25 20:38:31 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,5 @@
 # config.py
 
-import os
-from oauth2client.service_account import ServiceAccountCredentials
-
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-
-# Пытаемся прочитать путь из переменной окружения.
-# Если переменной нет — берём путь по умолчанию.
-CREDENTIALS_FILE = os.getenv("GOOGLE_APPLICATION_CREDENTIALS", "google-credentials.json")
-
-creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, scope)
-
 BOT_TOKEN = '7882594246:AAEkoqOtdJoA1hk7yBRKPiYchORNhvm4wNY'
 
 # ID и название Google Таблицы

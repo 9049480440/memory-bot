23ce5ad5d4e9f678020be134b54414263f78982f
9049480440 <9049480440ea@gmail.com>
2025-03-26 11:59:38 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-BOT_TOKEN = '7882594246:AAEkoqOtdJoA1hk7yBRKPiYchORNhvm4wNY'
+BOT_TOKEN = '7569888960:AAELmNsKxCPKxPilY1Ui6Ap09D_HmR0IwfY'
 
 # ID и название Google Таблицы
 SPREADSHEET_ID = '1_G8dY5NMro0cari71Be9eT9htiVgRWJ3rcQXfZUI_F8'

8586e8ce48572d7d792be94951f6ef540300830a
9049480440 <9049480440ea@gmail.com>
2025-03-26 14:02:22 +0500

    Update sheets.py

--- services/sheets.py
+++ services/sheets.py
@@ -23,24 +23,29 @@ client = gspread.authorize(creds)
 sheet = client.open_by_key(SPREADSHEET_ID).worksheet(ACTIVITY_SHEET_NAME)
 
 def add_or_update_user(user):
-    user_id = str(user.id)
-    all_users = sheet.get_all_values()
-    user_ids = [row[0] for row in all_users[1:]]  # пропускаем заголовок
+    try:
+        user_id = str(user.id)
+        all_users = sheet.get_all_values()
+        user_ids = [row[0] for row in all_users[1:]]  # пропускаем заголовок
 
-    if user_id in user_ids:
-        idx = user_ids.index(user_id) + 2
-        sheet.update_cell(idx, 4, '=TODAY()')
-    else:
-        new_row = [
-            str(user.id),
-            user.username or '',
-            user.full_name,
-            '=TODAY()',
-            'вход',
-            '',
-            '0'
-        ]
-        sheet.append_row(new_row)
+        if user_id in user_ids:
+            idx = user_ids.index(user_id) + 2
+            sheet.update_cell(idx, 4, '=TODAY()')
+        else:
+            new_row = [
+                str(user.id),
+                user.username or '',
+                user.full_name,
+                '=TODAY()',
+                'вход',
+                '',
+                '0'
+            ]
+            sheet.append_row(new_row)
+            print(f"[INFO] Новый пользователь добавлен: {user.full_name} ({user.id})")
+    except Exception as e:
+        print(f"[ERROR] Не удалось добавить пользователя {user.full_name} ({user.id}): {e}")
+
 
 # Новая функция для сохранения заявки
 def submit_application(user, date_text, location, monument_name):
